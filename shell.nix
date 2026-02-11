ac2ccd059bd42171d2dcd65ce1d83578377d9504
FrightenedFoxCN <FrightenedFox@outlook.com>
2026-02-11 11:13:38 +0800

    Fix Constans.hs, Try updating nixos

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/7e9b0dff974c89e070da1ad85713ff3c20b0ca97.tar.gz") {}, compiler ? "ghc8104" }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz") {}, compiler ? "ghc961" }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [ 
-    haskell.compiler.ghc8104
+    haskell.compiler.ghc961
     which
     (import ./default.nix { inherit pkgs compiler; })
   ];
